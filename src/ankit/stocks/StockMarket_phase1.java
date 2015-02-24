183da6f8050194caca3d57a7faaa9b0282944352
ankit-kapur <ankitkap@buffalo.edu>
2015-02-23 22:33:56 -0500

    Handled a blank line scenario in job 1

--- src/ankit/stocks/StockMarket_phase1.java
+++ src/ankit/stocks/StockMarket_phase1.java
@@ -16,31 +16,34 @@ public class StockMarket_phase1 {
 
 		public void map(LongWritable key, Text value, Context context)
 				throws IOException, InterruptedException {
-
+			String line = null;
 			/* --- Get the file name --- */
-			String fileName = ((FileSplit) context.getInputSplit()).getPath().getName();
+			String fileName = ((FileSplit) context.getInputSplit()).getPath()
+					.getName();
 
 			/* Remove the .csv from the file name */
 			if (fileName.indexOf(".csv") >= 0)
 				fileName = fileName.substring(0, fileName.indexOf(".csv"));
 
-			String line = value.toString();
-			/* A line of data: "2014-12-31,1.58,1.66,1.50,1.58,236500,1.58" */
-			
-			/* --- Get the date --- */
-			/* If this line is not a header, it won't contain a '-' */
-			if (line.indexOf('-') >= 0) {
-				String date = line.substring(0, line.indexOf(','));
-				String yearMonth = date.substring(0, date.lastIndexOf('-'));
+			if (value != null && (line = value.toString()).length() > 0) {
 				
-				/* They key will look like this: "ANTH-2014-12" */
-				key1.set(fileName + "-" + yearMonth);
-				/* The value will be the line as is */
-				value1.set(line);
-				
-				/* How a (key, value) pair will look: 
-				 * ("ANTH-2014-12", "2014-12-31,1.58,1.66,1.50,1.58,236500,1.58") */
-				context.write(key1, value1);
+				/* A line of data looks like this: "2014-12-31,1.58,1.66,1.50,1.58,236500,1.58" */
+
+				/* --- Get the date --- */
+				/* If this line is not a header, it won't contain a '-' */
+				if (line.indexOf('-') >= 0) {
+					String date = line.substring(0, line.indexOf(','));
+					String yearMonth = date.substring(0, date.lastIndexOf('-'));
+
+					/* They key will look like this: "ANTH-2014-12" */
+					key1.set(fileName + "-" + yearMonth);
+					/* The value will be the line as is */
+					value1.set(line);
+
+					/* How a (key, value) pair will look: ("ANTH-2014-12",
+					 * "2014-12-31,1.58,1.66,1.50,1.58,236500,1.58") */
+					context.write(key1, value1);
+				}
 			}
 		}
 	}
@@ -52,21 +55,22 @@ public class StockMarket_phase1 {
 
 		public void reduce(Text key, Iterable<Text> values, Context context)
 				throws IOException, InterruptedException {
-			
-			/* How an input (key, value) pair will look: 
-			 * ("ANTH-2014-12", "2014-12-31,1.58,1.66,1.50,1.58,236500,1.58") */
+
+			/* How an input (key, value) pair will look: ("ANTH-2014-12",
+			 * "2014-12-31,1.58,1.66,1.50,1.58,236500,1.58") */
 
 			int firstDay = 32;
 			int lastDay = 0;
-			String firstDayData = "", lastDayData = ""; 
-			
+			String firstDayData = "", lastDayData = "";
+
 			for (Text value : values) {
 				/* Get the DAY of the month */
 				String line = value.toString();
 				String date = line.substring(0, line.indexOf(','));
-				String dayOfTheMonth = date.substring(date.lastIndexOf('-')+1, date.length());
+				String dayOfTheMonth = date.substring(
+						date.lastIndexOf('-') + 1, date.length());
 				int day = Integer.parseInt(dayOfTheMonth);
-				
+
 				if (day < firstDay) {
 					firstDay = day;
 					firstDayData = line;
@@ -76,16 +80,19 @@ public class StockMarket_phase1 {
 					lastDayData = line;
 				}
 			}
-			
+
 			/* Now, we've found the data for the 1st and last day of the month.
 			 * Calculate xi */
-			double adjBegin = Double.parseDouble(firstDayData.substring(firstDayData.lastIndexOf(',')+1));
-			double adjEnd = Double.parseDouble(lastDayData.substring(lastDayData.lastIndexOf(',')+1));
-			
+			double adjBegin = Double.parseDouble(firstDayData
+					.substring(firstDayData.lastIndexOf(',') + 1));
+			double adjEnd = Double.parseDouble(lastDayData
+					.substring(lastDayData.lastIndexOf(',') + 1));
+
 			/* Apply the formula for xi */
-			double xi = (adjEnd - adjBegin)/adjBegin;
-			
-			/* Set the string as the same thing we received "STOCKNAME-MONTH-YEAR" */
+			double xi = (adjEnd - adjBegin) / adjBegin;
+
+			/* Set the string as the same thing we received
+			 * "STOCKNAME-MONTH-YEAR" */
 			key2.set(key.toString());
 			/* Set the value as xi */
 			value2.set(Double.toString(xi));
